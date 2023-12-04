acd71bd6617c56e7e3f5f001acb5483a775c600e
Miranda <mlivengd@gmail.com>
2023-12-03 21:32:05 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ import 'package:classmate/models/userModel.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+
+  /// Add data to Firebase
+  // Only needs to run if data doesn't already exist, or if changes were made to initializeData() in firebase.dart.
+  // Before re-running, delete existing data that you no longer want (will not override).
+  // await initializeData();
+
   runApp(const MyApp());
 }
 
